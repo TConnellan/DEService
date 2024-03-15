79944e9fc6bb42726d8986d4dcb6912191c4a303
TConnellan <thomasjconnellan@gmail.com>
2024-03-15 21:08:41 +1100

    endpoint defined

--- DEService/routes.jl
+++ DEService/routes.jl
@@ -1,5 +1,5 @@
-using Genie.Router, Genie.Requests, Genie
-using DEService.DESMain
+using Genie.Router, Genie.Requests, Genie, Genie.Renderer.Json
+using DEService.DESMain, DEService.DESSystem, DEService.DESState, DEService.DESParameters
 
 
 route("/") do
@@ -7,6 +7,26 @@ route("/") do
 end
 
 route("/compute") do
-  # test running simulations within container
-  return test_timing(100.0)
+  # endpoint will accept a value of lambda (float), use this to create a NetworkParameters object
+  λ = parse(Float64, getpayload(:lambda, 1))
+  params = create_scen1(λ)
+  # stateType = DESState.TrackTotals
+  stateType = getpayload(:mode, "")
+  if stateType == "tracktotals"
+    stateType = DESState.TrackTotals
+  elseif stateType == "trackalljobs"
+    stateType = DESState.TrackAllJobs
+  else
+    return "mode does not exist"
+  end
+  initState = DESSystem.create_init_state(stateType, params) 
+  initEvent = create_init_event(params, initState)
+  maxTime = parse(Float64, getpayload(:timethreshold, 100))
+  result = simulate(params, initState, initEvent, max_time = maxTime, callback = record_data)
+  if stateType == DESState.TrackTotals
+    return Genie.Renderer.Json.json(Dict("averageJobs" => result[1][1], "transitProportion" => result[1][2]))
+  end
+  if stateType == DESState.TrackAllJobs
+    return Genie.Renderer.Json.json(Dict("sojournTimes" => result))
+  end
 end
